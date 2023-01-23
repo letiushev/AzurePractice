ff222abbe77ddd485a8eb389880177509dbe00ae
Letiushev <223079929@ge.com>
2023-01-23 13:09:09 +0100

    added console app for filtering

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net.Http;
+using System.IO;
+using Newtonsoft.Json;
+using ConsoleApp1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Todos todos = new Todos();
+        // Webpage URL
+        string url = "https://jsonplaceholder.typicode.com/todos";
+        var client = new HttpClient();
+        var json = client.GetStringAsync(url).Result;
+        List<Todos> data = JsonConvert.DeserializeObject<List<Todos>>(json);
+        List<Todos> filtereData = new List<Todos>();
+
+        int userId = 1;
+        for (int i = 0; i < data.Count; i++)
+        {
+            if (data[i].userId == userId)
+            {
+                filtereData.Add(data[i]);
+            }
+            else
+            {
+                continue;
+            }
+        }
+
+        for (int i = 0; i < filtereData.Count; i++)
+        {
+            Console.WriteLine(filtereData[i].userId);
+        }
+    }
+}
+
