0336f73de770f28a6ec71d3ac28c275ba1f72ceb
Letiushev <223079929@ge.com>
2023-01-31 10:12:22 +0100

    added database test folder

---
+++ DatabaseTest/Program.cs
@@ -0,0 +1,21 @@
+﻿using Npgsql;
+
+string connectionString = "Host=localhost;Port=5432;Database=PostgreSQL 15;Username=postgres;Password=;";
+
+await using var conn = new NpgsqlConnection(connectionString);
+await conn.OpenAsync();
+
+// Insert some data
+await using (var cmd = new NpgsqlCommand("INSERT INTO data (some_field) VALUES (@p)", conn))
+{
+    cmd.Parameters.AddWithValue("p", "Hello world");
+    await cmd.ExecuteNonQueryAsync();
+}
+
+// Retrieve all rows
+await using (var cmd = new NpgsqlCommand("SELECT some_field FROM data", conn))
+await using (var reader = await cmd.ExecuteReaderAsync())
+{
+    while (await reader.ReadAsync())
+        Console.WriteLine(reader.GetString(0));
+}
