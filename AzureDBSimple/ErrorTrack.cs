d9a392cb0a8809897def4682b96ee76d77ad8946
Letiushev <223079929@ge.com>
2023-02-03 19:31:08 +0100

    added simple azure db

---
+++ AzureDBSimple/ErrorTrack.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AzureDB
+{
+    internal class ErrorTrack
+    {
+        public int Id;
+        public int ProductId;
+        public string ErrorType;
+        public ErrorTrack(int id, int productId, string errorType)
+        {
+            this.Id = id;
+            this.ProductId = productId;
+            this.ErrorType = errorType;
+        }
+        public ErrorTrack()
+        {
+        }
+    }
+}
